58b73d8d35da53b7f10814b6ce340fc08710f180
KaramSW <basLQRM@gmail.com>
2025-06-26 17:54:37 +0400

    Added log in with google functionality

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/services.dart';
 import 'navBar/nav_bar_main.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_projects/provider/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -394,7 +396,12 @@ class _LoginScreenState extends State<LoginScreen> {
                                     margin: const EdgeInsets.only(right: 10),
                                     child: ElevatedButton(
                                       onPressed: () {
-                                        // Handle Google login action
+                                        final provider =
+                                            Provider.of<GoogleSignInProvider>(
+                                              context,
+                                              listen: false,
+                                            );
+                                        provider.googleLogin();
                                       },
                                       style: ElevatedButton.styleFrom(
                                         elevation: 0,
@@ -430,7 +437,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                     margin: const EdgeInsets.only(right: 10),
                                     child: ElevatedButton(
                                       onPressed: () {
-                                        // Handle Google login action
+                                        // Handle Apple login action
                                       },
                                       style: ElevatedButton.styleFrom(
                                         elevation: 0,
